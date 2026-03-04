fbc3d721857bc57264cd66ef3290da0004f70104
K4ge <425630721@qq.com>
2026-03-04 22:16:44 +0800

    二分类pytorch学习测试

--- 20260304/Net类学习.py
+++ 20260304/Net类学习.py
@@ -1,16 +1,22 @@
 import torch
 import torch.nn as nn
-class Net(nn.Module):
 
+class Net(nn.Module):
     def __init__(self):
         super().__init__()
-
         self.fc1 = nn.Linear(2, 8)
-        self.fc2 = nn.Linear(8, 1)
+        self.bbb = nn.Linear(8, 1)
 
     def forward(self, x):
-
         x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-
+        x = self.bbb(x)
         return x
+
+model = Net()
+
+x = torch.randn(3, 2)   # 3个样本，每个2维特征
+y = model(x)
+
+print("x shape:", x.shape)
+print("y shape:", y.shape)
+print(model.parameters())
